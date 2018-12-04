ca5fa650eb60aab4b6381601f21b1e27c3a9e175
Ankon <44019225+luciferankon@users.noreply.github.com>
2018-12-04 10:36:07 +0530

    readFiles now returns a array of the given files contents

--- head.js
+++ head.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 let givenFiles = process.argv.slice(2);
 
 const readFiles = function(path){
-  return fs.readFileSync(path,'utf-8').split('\n');
+  return fs.readFileSync(path,'utf-8');
 }
 
 console.log(givenFiles.map(readFiles));
