8f06bfbe1ac029f89295bf303d4b91080fad910b
Ankon <44019225+luciferankon@users.noreply.github.com>
2018-12-07 15:33:11 +0530

    modified main

--- head.js
+++ head.js
@@ -1,9 +1,9 @@
-const { readFileSync, existsSync, lstatSync } = require("fs");
+const { fs } = require("fs");
 
 const { generateResult } = require("./src/lib.js");
 
 const { parser } = require("./src/head_IO.js");
 
 let parsedInputs = parser(process.argv.slice(2));
-let result = generateResult(readFileSync, existsSync, lstatSync, parsedInputs);
+let result = generateResult(fs, parsedInputs);
 console.log(result);
--- test/libTest.js
+++ test/libTest.js
@@ -1,148 +1,135 @@
-const assert = require("assert");
-const {
+const assert = require('assert');
+const { 
   generateResult,
   filterNumOfLine,
   filterNumOfChar,
   selectOperationType
-} = require("../src/lib.js");
+} = require('../src/lib.js');
 
-const isZero = function(element) {
-  return {
-    isFile: function() {
-      return element == 0;
-    }
+const isZero = function(element){
+  return {'isFile' : function(){
+          return element == 0;
+          }        
   };
-};
+}
 
-const complimentIdentity = function(element) {
+const complimentIdentity = function(element){
   return !element;
-};
-const add = function(first, second) {
-  return first + second;
-};
+}
+const add = function(first,second){
+  return first+ second;
+}
 
-describe("filterNumOfLine", function() {
-  let file = "node ./head.js -n5 file1\n";
-  file += "node ./head.js -n 5 file1\n";
-  file += "node ./head.js -5 file1\n";
-  file += "node ./head.js file1 file2\n";
-  file += "node ./head.js -n 5 file1 file2\n";
-  file += "node ./head.js -n5 file1 file2\n";
-  file += "node ./head.js -5 file1 file2\n";
-  file += "node ./head.js -c5 file1\n";
-  file += "node ./head.js -c 5 file1\n";
-  file += "node ./head.js -c5 file1 file2\n";
-  file += "node ./head.js -c 5 file1 file2\n";
+describe('filterNumOfLine',function(){
+  let file = 'node ./head.js -n5 file1\n';
+  file += 'node ./head.js -n 5 file1\n';
+  file += 'node ./head.js -5 file1\n';
+  file += 'node ./head.js file1 file2\n';
+  file += 'node ./head.js -n 5 file1 file2\n';
+  file += 'node ./head.js -n5 file1 file2\n';
+  file += 'node ./head.js -5 file1 file2\n';
+  file += 'node ./head.js -c5 file1\n';
+  file += 'node ./head.js -c 5 file1\n';
+  file += 'node ./head.js -c5 file1 file2\n';
+  file += 'node ./head.js -c 5 file1 file2\n';
 
-  it("should return 10 lines of the file if num of line is not specified ", function() {
-    let expectedOutput = "node ./head.js -n5 file1\n";
-    expectedOutput += "node ./head.js -n 5 file1\n";
-    expectedOutput += "node ./head.js -5 file1\n";
-    expectedOutput += "node ./head.js file1 file2\n";
-    expectedOutput += "node ./head.js -n 5 file1 file2\n";
-    expectedOutput += "node ./head.js -n5 file1 file2\n";
-    expectedOutput += "node ./head.js -5 file1 file2\n";
-    expectedOutput += "node ./head.js -c5 file1\n";
-    expectedOutput += "node ./head.js -c 5 file1\n";
-    expectedOutput += "node ./head.js -c5 file1 file2";
-    assert.deepEqual(filterNumOfLine(file), expectedOutput);
+  it('should return 10 lines of the file if num of line is not specified ',function(){
+  let expectedOutput = 'node ./head.js -n5 file1\n';
+  expectedOutput += 'node ./head.js -n 5 file1\n';
+  expectedOutput += 'node ./head.js -5 file1\n';
+  expectedOutput += 'node ./head.js file1 file2\n';
+  expectedOutput += 'node ./head.js -n 5 file1 file2\n';
+  expectedOutput += 'node ./head.js -n5 file1 file2\n';
+  expectedOutput += 'node ./head.js -5 file1 file2\n';
+  expectedOutput += 'node ./head.js -c5 file1\n';
+  expectedOutput += 'node ./head.js -c 5 file1\n';
+  expectedOutput += 'node ./head.js -c5 file1 file2';
+    assert.deepEqual(filterNumOfLine(file),expectedOutput);
+  });
+  
+  let expectedOutput = 'node ./head.js -n5 file1\n';
+  expectedOutput += 'node ./head.js -n 5 file1\n';
+  expectedOutput += 'node ./head.js -5 file1\n';
+  expectedOutput += 'node ./head.js file1 file2\n';
+  expectedOutput += 'node ./head.js -n 5 file1 file2';
+  it('should return specified no of lines if num of lines is specified',function(){
+    assert.deepEqual(filterNumOfLine(file,5),expectedOutput);
   });
 
-  let expectedOutput = "node ./head.js -n5 file1\n";
-  expectedOutput += "node ./head.js -n 5 file1\n";
-  expectedOutput += "node ./head.js -5 file1\n";
-  expectedOutput += "node ./head.js file1 file2\n";
-  expectedOutput += "node ./head.js -n 5 file1 file2";
-  it("should return specified no of lines if num of lines is specified", function() {
-    assert.deepEqual(filterNumOfLine(file, 5), expectedOutput);
-  });
-
-  it("should return empty string for 0 num of line", function() {
-    assert.deepEqual(filterNumOfLine(file, 0), "");
-  });
-});
-
-describe("filterNumOfChar", function() {
-  let file = "node ./head.js -n5 file1\n";
-  it("should give specified no of chars", function() {
-    assert.deepEqual(filterNumOfChar(file, 5), "node ");
+  it('should return empty string for 0 num of line',function(){
+    assert.deepEqual(filterNumOfLine(file,0),'');
   });
 });
 
-describe("selectOperationType", function() {
-  let file = "node ./head.js -n5 file1\n";
-  file += "node ./head.js -n 5 file1\n";
-  file += "node ./head.js -5 file1\n";
-  file += "node ./head.js file1 file2\n";
-  file += "node ./head.js -n 5 file1 file2\n";
-  file += "node ./head.js -n5 file1 file2\n";
-  file += "node ./head.js -5 file1 file2\n";
-  file += "node ./head.js -c5 file1\n";
-  file += "node ./head.js -c 5 file1\n";
-  file += "node ./head.js -c5 file1 file2\n";
-  file += "node ./head.js -c 5 file1 file2\n";
-  it("should return specified number of lines if type is n", function() {
-    let expectedOutput = "node ./head.js -n5 file1\n";
-    expectedOutput += "node ./head.js -n 5 file1\n";
-    expectedOutput += "node ./head.js -5 file1\n";
-    expectedOutput += "node ./head.js file1 file2\n";
-    expectedOutput += "node ./head.js -n 5 file1 file2";
-    assert.deepEqual(selectOperationType(file, 5, "n"), expectedOutput);
-  });
-  it("should return specified number of characters if type is c", function() {
-    let expectedOutput = "node ";
-    assert.deepEqual(selectOperationType(file, 5, "c"), expectedOutput);
-  });
-  it("should return 10 lines if type and number nothing is specified", function() {
-    let expectedOutput = "node ./head.js -n5 file1\n";
-    expectedOutput += "node ./head.js -n 5 file1\n";
-    expectedOutput += "node ./head.js -5 file1\n";
-    expectedOutput += "node ./head.js file1 file2\n";
-    expectedOutput += "node ./head.js -n 5 file1 file2\n";
-    expectedOutput += "node ./head.js -n5 file1 file2\n";
-    expectedOutput += "node ./head.js -5 file1 file2\n";
-    expectedOutput += "node ./head.js -c5 file1\n";
-    expectedOutput += "node ./head.js -c 5 file1\n";
-    expectedOutput += "node ./head.js -c5 file1 file2";
-    assert.deepEqual(selectOperationType(file), expectedOutput);
+describe('filterNumOfChar',function(){
+  let file = 'node ./head.js -n5 file1\n';
+  it('should give specified no of chars',function(){
+    assert.deepEqual(filterNumOfChar(file,5),'node ');
   });
 });
 
-describe("generateResult", function() {
-  describe("return error", function() {
-    it("should return an error if anything is wrong", function() {
-      let expectedOutput = "head: illegal line count -- -1";
-      let input = { type: "n", range: "-1", files: ["ankon"] };
-      let functions = {
-        readFileSync: add,
-        existsSync: complimentIdentity,
-        lstatSync: isZero
-      };
-      assert.deepEqual(generateResult(functions, input), expectedOutput);
+describe('selectOperationType',function(){
+  let file = 'node ./head.js -n5 file1\n';
+  file += 'node ./head.js -n 5 file1\n';
+  file += 'node ./head.js -5 file1\n';
+  file += 'node ./head.js file1 file2\n';
+  file += 'node ./head.js -n 5 file1 file2\n';
+  file += 'node ./head.js -n5 file1 file2\n';
+  file += 'node ./head.js -5 file1 file2\n';
+  file += 'node ./head.js -c5 file1\n';
+  file += 'node ./head.js -c 5 file1\n';
+  file += 'node ./head.js -c5 file1 file2\n';
+  file += 'node ./head.js -c 5 file1 file2\n';
+  it('should return specified number of lines if type is n',function(){
+    let expectedOutput = 'node ./head.js -n5 file1\n';
+    expectedOutput += 'node ./head.js -n 5 file1\n';
+    expectedOutput += 'node ./head.js -5 file1\n';
+    expectedOutput += 'node ./head.js file1 file2\n';
+    expectedOutput += 'node ./head.js -n 5 file1 file2';
+    assert.deepEqual(selectOperationType(file,5,'n'),expectedOutput);
+  });
+  it('should return specified number of characters if type is c',function(){
+    let expectedOutput = 'node ';
+    assert.deepEqual(selectOperationType(file,5,'c'),expectedOutput);
+  });
+  it('should return 10 lines if type and number nothing is specified',function(){
+    let expectedOutput = 'node ./head.js -n5 file1\n';
+    expectedOutput += 'node ./head.js -n 5 file1\n';
+    expectedOutput += 'node ./head.js -5 file1\n';
+    expectedOutput += 'node ./head.js file1 file2\n';
+    expectedOutput += 'node ./head.js -n 5 file1 file2\n';
+    expectedOutput += 'node ./head.js -n5 file1 file2\n';
+    expectedOutput += 'node ./head.js -5 file1 file2\n';
+    expectedOutput += 'node ./head.js -c5 file1\n';
+    expectedOutput += 'node ./head.js -c 5 file1\n';
+    expectedOutput += 'node ./head.js -c5 file1 file2';
+    assert.deepEqual(selectOperationType(file),expectedOutput);
+  });
+});
+
+describe('generateResult',function(){
+  describe('return error',function(){
+    it('should return an error if anything is wrong',function(){
+      let expectedOutput = 'head: illegal line count -- -1';
+      let input = {type: 'n', range:'-1',files: ['ankon']};
+      let functions = {readFileSync: add, existsSync: complimentIdentity, lstatSync: isZero};
+      assert.deepEqual(generateResult(functions,input),expectedOutput);
     });
   });
 
-  describe("test mock function for existsSync", function() {
-    it("should return the specified string if return value is false", function() {
-      let expectedOutput = "head: true: No such file or directory";
-      let input = { type: "n", range: "3", files: ["true"] };
-      let functions = {
-        readFileSync: add,
-        existsSync: complimentIdentity,
-        lstatSync: isZero
-      };
-      assert.deepEqual(generateResult(functions, input), expectedOutput);
+  describe('test mock function for existsSync',function(){
+    it('should return the specified string if return value is false',function(){
+      let expectedOutput = 'head: true: No such file or directory';
+      let input = {type: 'n', range:'3', files: ['true']};
+      let functions = {readFileSync: add, existsSync: complimentIdentity, lstatSync: isZero};
+      assert.deepEqual(generateResult(functions,input),expectedOutput);
     });
 
-    it("should not return the specified string if return value is true", function() {
-      let expectedOutput = "utf-8";
-      let input = { type: "n", range: "3", files: [""] };
-      let functions = {
-        readFileSync: add,
-        existsSync: complimentIdentity,
-        lstatSync: isZero
-      };
-      assert.deepEqual(generateResult(functions, input), expectedOutput);
+    it('should not return the specified string if return value is true',function(){
+      let expectedOutput = 'utf-8';
+      let input = {type: 'n', range:'3', files: ['']};
+      let functions = {readFileSync: add, existsSync: complimentIdentity, lstatSync: isZero};
+      assert.deepEqual(generateResult(functions,input),expectedOutput);
     });
   });
 });
