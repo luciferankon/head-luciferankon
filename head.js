0fb4f1258ee3e74db2a3667244d35c16b33b5059
Ankon <44019225+luciferankon@users.noreply.github.com>
2018-12-13 16:21:26 +0530

    refactored head.js

--- head.js
+++ head.js
@@ -4,6 +4,6 @@ const { generateResult } = require("./src/lib.js");
 
 const { parser } = require("./src/head_IO.js");
 
-let parsedInputs = parser(process.argv.slice(2));
-let result = generateResult(fs, parsedInputs, process.argv[1]);
+let parsedInputs = parser(process.argv.slice(1));
+let result = generateResult(fs, parsedInputs);
 console.log(result);
--- tail.js
+++ tail.js
@@ -5,5 +5,5 @@ const { generateResult } = require("./src/lib.js");
 const { parser } = require("./src/head_IO.js");
 
 let parsedInputs = parser(process.argv.slice(1));
-let result = generateResult(fs, parsedInputs, process.argv[1]);
+let result = generateResult(fs, parsedInputs);
 console.log(result);
