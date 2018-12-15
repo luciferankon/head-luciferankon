a891408e0d6cab9612f18efd8b279e6bbe30c231
Ankon <44019225+luciferankon@users.noreply.github.com>
2018-12-15 10:16:19 +0530

    moved error messages to const variables

--- src/errorLib.js
+++ src/errorLib.js
@@ -1,10 +1,17 @@
+const HEADOPTION = 'head: option requires an argument -- ';
+const HEADUSAGE = 'usage: head [-n lines | -c bytes] [file ...]';
+const HEADILLEGALOPTION = 'head: illegal option -- ';
+const TAILOPTION = 'tail: option requires an argument -- ';
+const TAILUSAGE = 'usage: tail [-F | -f | -r] [-q] [-b # | -c # | -n #] [file ...]';
+const TAILILLEGALOPTION = 'tail: illegal option -- ';
+
 const errorCheckHead = function(type, value, files) {
   let errorWord = {
     n: "line",
     c: "byte"
   };
   if (isTypeError(type)) {
-    return "head: illegal option -- " + type + "\nusage: head [-n lines | -c bytes] [file ...]";
+    return HEADILLEGALOPTION + type + '\n' + HEADUSAGE;
   }
 
   if (isValueError(value)) {
@@ -12,19 +19,19 @@ const errorCheckHead = function(type, value, files) {
   }
 
   if (isFileError(files)) {
-    return "head: option requires an argument -- " + type + "\nusage: head [-n lines | -c bytes] [file ...]";
+    return HEADOPTION + type + '\n' + HEADUSAGE;
   }
 };
 
 const errorCheckTail = function(type, value, files) {
   if (isTypeError(type)) {
-    return "tail: illegal option -- " + type + "\nusage: tail [-F | -f | -r] [-q] [-b # | -c # | -n #] [file ...]";
+    return TAILILLEGALOPTION + type + '\n' + TAILUSAGE;
   }
 
   if (isFileError(files)) {
-    return "tail: option requires an argument -- " + type + "\nusage: tail [-F | -f | -r] [-q] [-b # | -c # | -n #] [file ...]";
+    return TAILOPTION + type + '\n' + TAILUSAGE;
   }
-  
+
   if (isValueError(value + 1)) {
     return "tail: illegal offset -- " + value;
   }
