4a9ad2b917aef71de73fce610482dedaf8d7a6fa
Ankon <44019225+luciferankon@users.noreply.github.com>
2018-12-15 10:08:46 +0530

    removed prettier effect from errorLib

--- src/errorLib.js
+++ src/errorLib.js
@@ -4,39 +4,27 @@ const errorCheckHead = function(type, value, files) {
     c: "byte"
   };
   if (isTypeError(type)) {
-    return (
-      "head: illegal option -- " +
-      type +
-      "\nusage: head [-n lines | -c bytes] [file ...]"
-    );
+    return "head: illegal option -- " + type + "\nusage: head [-n lines | -c bytes] [file ...]";
   }
+
   if (isValueError(value)) {
     return "head: illegal " + errorWord[type] + " count -- " + value;
   }
+
   if (isFileError(files)) {
-    return (
-      "head: option requires an argument -- " +
-      type +
-      "\nusage: head [-n lines | -c bytes] [file ...]"
-    );
+    return "head: option requires an argument -- " + type + "\nusage: head [-n lines | -c bytes] [file ...]";
   }
 };
 
 const errorCheckTail = function(type, value, files) {
   if (isTypeError(type)) {
-    return (
-      "tail: illegal option -- " +
-      type +
-      "\nusage: tail [-F | -f | -r] [-q] [-b # | -c # | -n #] [file ...]"
-    );
+    return "tail: illegal option -- " + type + "\nusage: tail [-F | -f | -r] [-q] [-b # | -c # | -n #] [file ...]";
   }
+
   if (isFileError(files)) {
-    return (
-      "tail: option requires an argument -- " +
-      type +
-      "\nusage: tail [-F | -f | -r] [-q] [-b # | -c # | -n #] [file ...]"
-    );
+    return "tail: option requires an argument -- " + type + "\nusage: tail [-F | -f | -r] [-q] [-b # | -c # | -n #] [file ...]";
   }
+  
   if (isValueError(value + 1)) {
     return "tail: illegal offset -- " + value;
   }
