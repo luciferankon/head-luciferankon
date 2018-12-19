80217920d222dc08e80256f1dd9595c1e0fd8f53
Ankon <44019225+luciferankon@users.noreply.github.com>
2018-12-19 10:52:11 +0530

    reanamed lib to fileUtils and
    inputOutputLib to IO

--- head.js
+++ head.js
@@ -1,8 +1,8 @@
 const fs = require("fs");
 
-const { generateResult } = require("./src/lib.js");
+const { generateResult } = require("./src/fileUtils.js");
 
-const { parser } = require("./src/inputOutputLib.js");
+const { parser } = require("./src/IO.js");
 
 let parsedInputs = parser(process.argv.slice(1));
 let result = generateResult(fs, parsedInputs);
